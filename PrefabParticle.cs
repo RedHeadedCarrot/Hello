cd347ddc037462405a3893b92398438e87e2bccd
RedHeadedCarrot <sdavis2007@unm.edu>
2026-09-04 12:00:43 -0600

    Update PrefabParticle.cs

--- PrefabParticle.cs
+++ PrefabParticle.cs
@@ -1,16 +1,48 @@
-using UnityEngine;
 
-public class PrefabParticle : MonoBehaviour
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Reference to the player GameObject.
+    public GameObject player;
+
+    // The distance between the camera and the player.
+    public Vector3 offset;
+
+    public Vector2 camRotation = new Vector2(45f, 0f);
+    public Vector2 input = new Vector2();
+
+    public float rotationSpeed = 10f;
+
+    // Start is called before the first frame update.
     void Start()
     {
-        
+        // Calculate the initial offset between the camera's position and the player's position.
+        offset = transform.position - player.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    // LateUpdate is called once per frame after all Update functions have been completed.
+    void LateUpdate()
     {
-        
+        // Orbit camera based on target
+        Quaternion lookRotation = Quaternion.Euler(camRotation);
+        Vector3 lookDirection = lookRotation * Vector3.forward;
+        Vector3 lookPosition = player.transform.position - lookDirection * offset.magnitude;
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
+        camRotation += rotationSpeed * Time.deltaTime * input;
+        if(p==null)
+        return
+    }
+
+
+
+    private void OnLook(InputValue inputValue)
+    {
+        Vector2 tInput = inputValue.Get<Vector2>();
+        input.x = tInput.y;
+        input.y = tInput.x;
+
     }
 }
